10823bfca68eb18ade781955e18b9fd0b749230c
Taras <mythbuster.lviv@gmail.com>
2021-02-07 20:49:50 +0200

    Initial commit

---
+++ src/com/company/Lesson2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Lesson2 {
+    static String tar="Hello World";
+    Lesson2(){
+        System.out.println("Hello");
+    }
+}
---
+++ src/com/company/Lesson3.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Lesson3 extends Lesson2 {
+    public static int n;
+    Lesson3(){
+        System.out.println(tar);
+        }
+
+    public static void Output(){
+        System.out.println(n);
+    }
+    public static void Taras(){
+        System.out.println("Hello Taras");
+    }
+}
